36cb1ababb6b260a7a07d67355e54154698681d9
jewel <snowdecember079@gmail.com>
2022-04-30 22:26:54 -0400

    finish

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  const reversedWord = reverse(word);
+ 
+  return word === reversedWord;
 }
 
 /* 
